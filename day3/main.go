f016ce5c93ba305531a2ca0b9e7da73e40a55998
Chirag Joshi <chirag.joshi@orbis.com>
2022-12-03 21:51:07 +1100

    day 3 - rucksack reorganization - part two

--- day3/main.go
+++ day3/main.go
@@ -12,13 +12,21 @@ type Rucksack struct {
   CompartmentTwo string
 }
 
+func (r Rucksack) Length() int {
+  return len(r.CompartmentOne) + len(r.CompartmentTwo)
+}
+
+func (r Rucksack) AllItems() string {
+  return r.CompartmentOne + r.CompartmentTwo
+}
+
 const priorityString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func GetPriority(item string) int {
+func getPriority(item string) int {
   return strings.Index(priorityString, item) + 1
 }
 
-func GetMisplacedItem(rucksack Rucksack) string {
+func getMisplacedItem(rucksack Rucksack) string {
   for i := 0; i < len(rucksack.CompartmentOne); i++ {
     item := string(rucksack.CompartmentOne[i])
     if strings.Index(rucksack.CompartmentTwo, item) > -1 {
@@ -28,7 +36,7 @@ func GetMisplacedItem(rucksack Rucksack) string {
   return "0" // should never get here according to data specifications
 }
 
-func ReadInputFile(filename string) []Rucksack {
+func readInputFile(filename string) []Rucksack {
   file, err := os.Open(filename)
   if err != nil {
     fmt.Println(err)
@@ -49,16 +57,52 @@ func ReadInputFile(filename string) []Rucksack {
   return rucksacks
 }
 
-func PartOne() {
+func getRucksackWithMostItems(rucksacks []Rucksack) Rucksack {
+  maxLen := -1
+  var rucksackWithMostItems Rucksack
+  for _, rucksack := range rucksacks {
+    if rucksack.Length() > maxLen {
+      maxLen = rucksack.Length()
+      rucksackWithMostItems = rucksack
+    }
+  }
+  return rucksackWithMostItems
+}
+
+func PartOne(rucksacks []Rucksack) {
+  sumOfPriorities := 0
+  for _, rucksack := range rucksacks {
+    misplacedItem := getMisplacedItem(rucksack)
+    sumOfPriorities += getPriority(misplacedItem)
+  }
+  fmt.Println(sumOfPriorities)
+}
   
+func PartTwo(rucksacks []Rucksack) {
+  sumOfPriorities := 0
+
+  for i := 0 ; i < len(rucksacks); i+=3 {
+    group := rucksacks[i:i+3]
+    rucksackWithMostItems := getRucksackWithMostItems(group)
+
+    for _, item := range rucksackWithMostItems.AllItems() {
+      strItem := string(item)
+      isInFirstRucksack := strings.Index(group[0].AllItems(), strItem) != -1
+      isInSecondRucksack := strings.Index(group[1].AllItems(), strItem) != -1
+      isInThirdRucksack := strings.Index(group[2].AllItems(), strItem) != -1
+      if isInFirstRucksack && isInSecondRucksack && isInThirdRucksack {
+        sumOfPriorities += getPriority(strItem)
+        break
+      }
+    }
+  }
+  
+  fmt.Println(sumOfPriorities)
 }
 
 func main() {
-  rucksacks := ReadInputFile("input.txt")
-  sumOfPriorities := 0
-  for _, rucksack := range rucksacks {
-    misplacedItem := GetMisplacedItem(rucksack)
-    sumOfPriorities += GetPriority(misplacedItem)
-  }
-  fmt.Println(sumOfPriorities)
+  rucksacks := readInputFile("input.txt")
+
+  PartOne(rucksacks)
+  PartTwo(rucksacks)
 }
