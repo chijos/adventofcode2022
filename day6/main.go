02f08357f4558dee62149e62b9f04c94cd5b4332
Chirag Joshi <chirag.joshi@orbis.com>
2022-12-06 23:47:26 +1100

    day 6 - tuning trouble - part one

---
+++ day6/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readInputFile(filename string) string {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+  scanner.Scan()
+  return scanner.Text()
+}
+
+func hasRepeatedCharacters(packet string) bool {
+  for i := 0; i < len(packet) - 1; i++ {
+    character := string(packet[i])
+    for j := i + 1; j < len(packet); j++ {
+      otherCharacter := string(packet[j])
+      if otherCharacter == character {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+func partOne(dataStream string) {
+  for i := 0; i < len(dataStream) - 4; i++ {
+    packet := dataStream[i:i+4]
+    if !hasRepeatedCharacters(packet) {
+      fmt.Println(i + 4)
+      return
+    }
+  }
+}
+
+func partTwo() {}
+
+func main() {
+	dataStream := readInputFile("input.txt")
+	partOne(dataStream)
+	partTwo()
+}
